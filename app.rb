0ffcd709676501b848cc56fe021d59c595399cfd
Ivar Vong <ivar@ivarvong.com>
2015-05-05 14:49:18 -0400

    pix_fmt..?

--- app.rb
+++ app.rb
@@ -7,32 +7,28 @@ get '/' do
 	redirect "/mp4?url=http://bukk.it/wut.gif"
 end
 
-def convert(gif_file, mp4_file)
-	`ffmpeg -f gif -i #{gif_file} #{mp4_file}`
-rescue
-	nil
-end
-
 get '/mp4' do
-	start_time = Time.now.utc.to_f
-	url = params['url']
-	puts "Converting #{url}"
+	begin
+		start_time = Time.now.utc.to_f
+		url = params['url']
+		puts "Converting #{url}"
 
-	gif_file = Tempfile.new('gif').path + ".gif"
-	File.open(gif_file, 'wb') do |f|
-		f.write(HTTParty.get(url).body)
-	end
-	mp4_file = Tempfile.new('mp4').path + ".mp4"
+		gif_file = Tempfile.new('gif').path + ".gif"
+		File.open(gif_file, 'wb') do |f|
+			f.write(HTTParty.get(url).body)
+		end
+		mp4_file = Tempfile.new('mp4').path + ".mp4"
 
-	if convert(gif_file, mp4_file)
+		`ffmpeg -i "#{gif_file}" -pix_fmt yuv420p "#{mp4_file}"`
+
 		headers "Cache-Control" => "public, max-age=300"
 		headers "X-Convert-Time" => "#{(Time.now.utc.to_f - start_time).round(2)}"
 		content_type "video/mp4"
 		File.open(mp4_file, 'rb').read
-	else
+
+	rescue
 		status_code 500
 		content_type "text/plain"
 		"Error converting"
 	end
-	
 end
